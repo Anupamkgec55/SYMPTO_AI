fdc14f1228bba9a144a52fc11569a3d39ad8aaac
Anupamkgec55 <127596602+Anupamkgec55@users.noreply.github.com>
2024-06-22 21:46:48 +0530

    Add files via upload

---
+++ CollegeProject/Anupam/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.Home,name='home'),
+    path('home/',views.Home,name='home'),
+    path('heart/',views.Welcome,name='heart'),
+   path('Departments/liver/', views.Liver, name='liver'),
+   path('diabetes/',views.Welcome,name='diabetes'),
+    path('cancer/',views.Welcome,name='cancer'),
+    # path('Departments/liver/user/',views.User,name='user'),
+    path('about/',views.About,name='about'),
+    path('Departments/',views.Dept,name='dept'),
+    path('Contacts/',views.Cont,name='cont'),
+]
